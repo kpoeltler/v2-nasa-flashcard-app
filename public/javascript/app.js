2883baddeff67c0302a64ca5d1134737b526a963
Katie Poeltler <kpoeltler@gmail.com>
2018-09-26 01:10:37 -0500

    v3 rollback point

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,6 +15,24 @@ $(document).ready(function () {
     );
   });
 
+  // START DATE SCRIPT
+
+var showdate	= "yes"  	// SHOW THE DATE | yes | no |
+
+if (showdate == "yes") {
+let date = $("<div>");
+var d=new Date();
+var weekday=new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+var monthname=new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+document.write('<span class="date-font">' + weekday[d.getDay()] + ' ');
+document.write(monthname[d.getMonth()] + ' ');
+document.write(d.getDate() + '. ');
+document.write(d.getFullYear())
+document.write('<br></span>')
+document.write('</div>');
+$("#date-location").html(date);
+};
+
 
 //   * When the user clicks on the button,
 // toggle between hiding and showing the dropdown content */
