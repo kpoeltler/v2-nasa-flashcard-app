d2f2c21d58d726154a18e68efa3d86abc52e4181
Katie Poeltler <kpoeltler@gmail.com>
2018-09-25 20:51:30 -0500

    added edit model and arranged the img and description into two columns

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   let state = {
     cardArr: [],
     current: 0,
@@ -6,14 +6,35 @@ $(document).ready(function() {
     currentSubject: -1
   };
 
-  $(document).ready(function(){
-    $("#logo-fade").animate({opacity: '1', display: 'block'}, 3000);
-    $("#logo-fade").animate({opacity: '0', display: 'none', position: 'relative'}, 3000, 
-        function () {
-          $(this).css('z-index', -10)
-        }
+  $(document).ready(function () {
+    $("#logo-fade").animate({ opacity: '1', display: 'block' }, 3000);
+    $("#logo-fade").animate({ opacity: '0', display: 'none', position: 'relative' }, 3000,
+      function () {
+        $(this).css('z-index', -10)
+      }
     );
   });
+
+
+//   * When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("menuDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
 
   /**
    * Renders a picture and paragraph onto index.html
@@ -22,41 +43,47 @@ $(document).ready(function() {
   const render = () => {
     let apodPicture = $("<img>");
     apodPicture.attr("id", "db-picture");
-    console.log("current",  state.current);
-    apodPicture.attr("src", state.cardArr[0][state.current].hdurl);
+    console.log("current", state.current);
+    apodPicture.attr("src", state.cardArr[ 0 ][ state.current ].hdurl);
     $("#card").html(apodPicture);
   };
-  $("#card").click(function() {
-      if (
-        $("#card")
-          .children()
-          .is("p")
-        ) 
-    {
-        let apodPicture = $("<img>");
-        apodPicture.attr("src", state.cardArr[0][state.current].hdurl);
-        apodPicture.attr("id", "db-picture");
-        $("#card").html(apodPicture);
+
+
+  $("#card").click(function () {
+    if (
+      $("#card")
+        .children()
+        .is("p")
+    ) {
+      let apodPicture = $("<img>");
+      apodPicture.attr("src", state.cardArr[ state.current ].hdurl);
+      // apodPicture.attr("src", state.cardArr[0][ state.current ].hdurl);
+      apodPicture.attr("id", "db-picture");
+      $("#card").html(apodPicture);
 
     } else {
-        let apodExplanation = $("<p>");
-        $("#card").html(
-            apodExplanation.text(state.cardArr[0][state.current].explanation)
-        )
-        let userExplanation = $("<p>");
-        $("#card").append(
-            userExplanation.text(state.cardArr[0][state.current].user_desc) 
-        )
-        $("#card").append(
-            "<button id='edit-btn' data-toggle='modal' data-target='#myModal'> <i class='material-icons'>&#xe254;</i> </button>"
-        );  
-    }
-    
+      let apodExplanation = $("<p>");
+      $("#card").html(
+        apodExplanation.text(state.cardArr[ state.current ].explanation)
+        // apodExplanation.text(state.cardArr[ 0 ][ state.current ].explanation)
+      )
+      let userExplanation = $("<p>");
+      $("#card").append(
+        userExplanation.text(state.cardArr[ state.current ].user_desc)
+        // userExplanation.text(state.cardArr[ 0 ][ state.current ].user_desc)
+      )
 
-    
+      $("#card").append(
+        "<button id='edit-btn' data-toggle='modal' data-target='#myModal'> <i class='material-icons'>&#xe254;</i> </button>"
+      );
+    }
+  });
+
+
+
 
   //=====================================================================
-  $.get("api/all", function(data) {
+  $.get("api/all", function (data) {
     state.cardArr = updateArr(state.cardArr, data);
     console.log(state.cardArr);
     render();
@@ -115,72 +142,72 @@ $(document).ready(function() {
   const resetCount = () => 0;
 
 
-  /** 
+  /**
    * Event Listens on specific Buttons
    * Next and Previous
   */
-  $("#nextbtn").on("click", function() {
-    if(state.current != state.cardArr[0].length -1){
-    state.current = increment(state.current);
+  $("#nextbtn").on("click", function () {
+    if (state.current != state.cardArr[ 0 ].length - 1) {
+      state.current = increment(state.current);
     }
     render();
   });
 
-  $("#prebtn").on("click", function() {
+  $("#prebtn").on("click", function () {
     console.log("cardlength", state);
-    if(state.current != 0){
-    state.current = decrement(state.current);
+    if (state.current != 0) {
+      state.current = decrement(state.current);
     }
     render();
   });
 
-/**
- * Event listener on button #add-comment-btn
- * When #add-comment-btn is clicked
- * Get content entered from #user-comment input
- * Invoke function addUserComment passing the 
- * content from input field
- */
-$("#add-comment-btn").on( "click", function(event) {
-  event.preventDefault();
-  // get value from #user-comment 
-  var userCommentObj = {
-    id: state.cardArr[0][state.current].id,
-    user_desc: $("#user-comment").val().trim()
-}
+  /**
+   * Event listener on button #add-comment-btn
+   * When #add-comment-btn is clicked
+   * Get content entered from #user-comment input
+   * Invoke function addUserComment passing the
+   * content from input field
+   */
+  $("#add-comment-btn").on("click", function (event) {
+    event.preventDefault();
+    // get value from #user-comment
+    var userCommentObj = {
+      id: state.cardArr[ 0 ][ state.current ].id,
+      user_desc: $("#user-comment").val().trim()
+    }
 
     addUserComment(userCommentObj);
-});
+  });
 
-/**
- * @function 
- * @param {string} userComment - comment user enter
- * Send user comment
- * Get specific (current card ID)
- * Send (current card ID) and Comment via Ajax
- * ID will be matched with db card row 
- * Then Comment added into column user_desc
- */
-var addUserComment = function (comment) {  
+  /**
+   * @function
+   * @param {string} userComment - comment user enter
+   * Send user comment
+   * Get specific (current card ID)
+   * Send (current card ID) and Comment via Ajax
+   * ID will be matched with db card row
+   * Then Comment added into column user_desc
+   */
+  var addUserComment = function (comment) {
 
 
 
-  var queryURL = "api/description/" + comment.id;
+    var queryURL = "api/description/" + comment.id;
 
-  $.ajax({
+    $.ajax({
       type: 'PUT',
       url: queryURL,
       data: comment,
-      success: function(data){
-          console.log("success");
-          console.log(data);
+      success: function (data) {
+        console.log("success");
+        console.log(data);
       },
-      error: function(data){
-          console.log("error");
-          console.log(data);
+      error: function (data) {
+        console.log("error");
+        console.log(data);
       }
-  });
-};
+    });
+  };
 
 });
 
@@ -191,7 +218,7 @@ var addUserComment = function (comment) {
 * Set all to empty string - Just incase any field is empty
 * Send via Ajax
 */
-var addAPIDaily = function (theResult) {  
+var addAPIDaily = function (theResult) {
 
   // Set to empty string incase any are missing
   var copyright = "";
@@ -205,42 +232,42 @@ var addAPIDaily = function (theResult) {
 
   // Constructing a full card object from the api call to database
   var addAPIDailyObj = {
-      copyright: copyright,
-      date: date,
-      explanation: explanation,
-      hdurl: hdurl,
-      media_type: media_type,
-      service_version: service_version,
-      title: title,
-      url: url,
+    copyright: copyright,
+    date: date,
+    explanation: explanation,
+    hdurl: hdurl,
+    media_type: media_type,
+    service_version: service_version,
+    title: title,
+    url: url,
   };
 
   // Match API with Column Names
   // The key1 or key2 is the key
   // addAPIDailyObj[key1] is the value
   // theResult[key2] is the value
-  Object.keys(addAPIDailyObj).forEach(function(key1) {
-      Object.keys(theResult).forEach(function(key2) {
-          if( key1 === key2 ) {
-              addAPIDailyObj[key1] = theResult[key2];
-          }
-      });
-  }); 
+  Object.keys(addAPIDailyObj).forEach(function (key1) {
+    Object.keys(theResult).forEach(function (key2) {
+      if (key1 === key2) {
+        addAPIDailyObj[ key1 ] = theResult[ key2 ];
+      }
+    });
+  });
 
   var queryURL = "/api/insert-card";
 
   $.ajax({
-      type: 'POST',
-      url: queryURL,
-      data: addAPIDailyObj,
-      success: function(data){
-          console.log("success");
-          console.log(data);
-      },
-      error: function(data){
-          console.log("error");
-          console.log(data);
-      }
+    type: 'POST',
+    url: queryURL,
+    data: addAPIDailyObj,
+    success: function (data) {
+      console.log("success");
+      console.log(data);
+    },
+    error: function (data) {
+      console.log("error");
+      console.log(data);
+    }
   });
 };
 
@@ -248,18 +275,18 @@ var addAPIDaily = function (theResult) {
 * Get API Call via Ajax every 24 hours - Using the setInterval
 * @function
 */
-var getAPI = function() {
+var getAPI = function () {
   let apod = "https://api.nasa.gov/planetary/apod?api_key=VBmmkpWMV3eWpklLC1tsXUmUiiej1unTpiihHq8n";
   let success = $.ajax({
-      url: apod,
-      method: "GET",
-      success: function(result) {
-          addAPIDaily(result);
-      },
-      error: function(result) {
-          console.log("error");
-          console.log(result);
-      }
+    url: apod,
+    method: "GET",
+    success: function (result) {
+      addAPIDaily(result);
+    },
+    error: function (result) {
+      console.log("error");
+      console.log(result);
+    }
   })
 };
 
@@ -267,7 +294,7 @@ var getAPI = function() {
 * Timer to invoke API call every 24 hours - Invoke addAPIDaily();
 * 1 day = 86,400,000 ms
 */
-setInterval(function() {
+setInterval(function () {
   var getApiCall = "https://api.nasa.gov/planetary/apod?api_key=VBmmkpWMV3eWpklLC1tsXUmUiiej1unTpiihHq8n";
   getAPI();
 }, 86400000); // Every 24hrs
@@ -279,7 +306,7 @@ setInterval(function() {
 * @function
 * @param
 */
-var checksTogether = function(subcheckboxCode){
+var checksTogether = function (subcheckboxCode) {
   var subcheckbox = subcheckboxCode;
   $('#subject-group').append(subcheckbox);
 };
@@ -288,26 +315,26 @@ var checksTogether = function(subcheckboxCode){
 * GET cards of all subjects
 * @function
 */
-var getAllSubjects = function() {
+var getAllSubjects = function () {
 
   var queryURL = "/api/subjectsall";
-  
+
   let allSubArr;
   let subcheckbox;
 
   let success = $.ajax({
-      url: queryURL,
-      method: "GET",
-      success: function(result) {
-          for (var i = 0; i < result.length; i++)  {
-                  allSubArr = result[i].subject;
-                  subcheckbox = "<input type='checkbox' class='subject-Check' id='" + allSubArr + "'> <span class='check-name'>" +
-                  allSubArr + "<span>"
-                  checksTogether(subcheckbox);
-          }
-      },
-      error: function(result) {
+    url: queryURL,
+    method: "GET",
+    success: function (result) {
+      for (var i = 0; i < result.length; i++) {
+        allSubArr = result[ i ].subject;
+        subcheckbox = "<input type='checkbox' class='subject-Check' id='" + allSubArr + "'> <span class='check-name'>" +
+          allSubArr + "<span>"
+        checksTogether(subcheckbox);
       }
+    },
+    error: function (result) {
+    }
   });
 };
 
@@ -317,27 +344,27 @@ var url = "https://api.nasa.gov/planetary/apod?api_key=VBmmkpWMV3eWpklLC1tsXUmUi
 
 $.ajax({
   url: url,
-  success: function(result){
-  if("copyright" in result) {
-    $("#copyright").text("Image Credits: " + result.copyright);
-  }
-  else {
-    $("#copyright").text("Image Credits: " + "Public Domain");
-  }
+  success: function (result) {
+    if ("copyright" in result) {
+      $("#copyright").text("Image Credits: " + result.copyright);
+    }
+    else {
+      $("#copyright").text("Image Credits: " + "Public Domain");
+    }
 
-  if(result.media_type == "video") {
-    $("#apod_img_id").css("display", "none");
-    $("#apod_vid_id").attr("src", result.url);
+    if (result.media_type == "video") {
+      $("#apod_img_id").css("display", "none");
+      $("#apod_vid_id").attr("src", result.url);
+    }
+    else {
+      $("#apod_vid_id").css("display", "none");
+      $("#apod_img_id").attr("src", result.url);
+    }
+    $("#reqObject").text(url);
+    $("#returnObject").text(JSON.stringify(result, null, 4));
+    $("#apod_explaination").text(result.explanation);
+    $("#apod_title").text(result.title);
   }
-  else {
-    $("#apod_vid_id").css("display", "none");
-    $("#apod_img_id").attr("src", result.url);
-  }
-  $("#reqObject").text(url);
-  $("#returnObject").text(JSON.stringify(result, null, 4));
-  $("#apod_explaination").text(result.explanation);
-  $("#apod_title").text(result.title);
-}
 });
 
 let apod = "https://api.nasa.gov/planetary/apod?api_key=VBmmkpWMV3eWpklLC1tsXUmUiiej1unTpiihHq8n";
@@ -345,14 +372,14 @@ let apod = "https://api.nasa.gov/planetary/apod?api_key=VBmmkpWMV3eWpklLC1tsXUmU
 let success = $.ajax({
   url: apod,
   method: "GET",
-  success: function(result){
-  if("copyright" in result) {
-    $("#copyright").text("Image Credits: " + result.copyright);
+  success: function (result) {
+    if ("copyright" in result) {
+      $("#copyright").text("Image Credits: " + result.copyright);
+    }
+    else {
+      $("#copyright").text("Image Credits: " + "Public Domain");
+    }
   }
-  else {
-    $("#copyright").text("Image Credits: " + "Public Domain");
-  }
-}
 })
 }
 )
